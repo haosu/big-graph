33b51f5c0fb96d3483c9f73f993148b9f5fc6101
haosu <me@haosu.org>
2014-03-28 16:58:19 -0700

    Clean up routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,4 @@
-var Entity = require('../models/entity.js');
-
 /* GET home page. */
 exports.index = function(req, res){
-  console.log(Entity);
-  var entityPromise = Entity.create({data: 'some shit'}, function(a, b) {
-  });
-
-  Entity.find({}, function(error, entity) {
-    console.log(entity);
-  });
   res.render('index', { title: 'Express' });
 };
